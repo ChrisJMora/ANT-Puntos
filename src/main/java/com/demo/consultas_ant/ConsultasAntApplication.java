3b71115d2df71165e2c10a3c5dce9a77b1c6238a
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2024-05-27 21:49:17 -0500

    Initial Commit

---
+++ src/main/java/com/demo/consultas_ant/ConsultasAntApplication.java
@@ -0,0 +1,13 @@
+package com.demo.consultas_ant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultasAntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultasAntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/consultas_ant/ConsultasAntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.consultas_ant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultasAntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
