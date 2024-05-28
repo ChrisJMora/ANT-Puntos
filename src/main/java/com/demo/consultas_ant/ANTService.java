d9228e65f63cd4b243f3fe75828972a67377370f
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2024-05-28 12:25:48 -0500

    Se refactoriza el código

---
+++ src/main/java/com/demo/consultas_ant/ANTService.java
@@ -0,0 +1,36 @@
+package com.demo.consultas_ant;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class ANTService {
+
+    private final RestTemplate _restTemplate = new RestTemplate();
+
+    public Response LicencePoints(String id)
+    {
+        ResponseEntity<String> response = _restTemplate.getForEntity(GetURL(id), String.class);
+        String htmlContent = response.getBody();
+        if(htmlContent == null) return null;
+        String licencePoints = SearchPhrase(htmlContent, "Información adicional de Puntos", 2, 2);
+        return new Response(licencePoints);
+    }
+
+    private String GetURL(String id)
+    {
+        String url_template = "https://consultaweb.ant.gob.ec/PortalWEB/paginas/clientes/clp_grid_citaciones.jsp?ps_tipo_identificacion=CED&ps_identificacion={id}&ps_placa=";
+        return url_template.replace("{id}", id);
+    }
+
+    private String SearchPhrase(String htmlContent, String phrase, int offset, int targetWidth)
+    {
+        int index = htmlContent.indexOf(phrase) + phrase.length() + offset;
+        String result;
+        try {
+            result = htmlContent.substring(index, index + targetWidth);
+        } catch (Exception ex) {
+            result = ex.getMessage();
+        }
+        return result;
+    }
+}
--- src/main/java/com/demo/consultas_ant/Response.java
+++ src/main/java/com/demo/consultas_ant/Response.java
@@ -1,19 +1,9 @@
 package com.demo.consultas_ant;
 
 public class Response {
-    private String puntos;
+    public String licencePoints;
 
-    public Response() {}
-
-    public Response(String resultado) {
-        this.puntos = resultado;
-    }
-
-    public String getPuntos() {
-        return puntos;
-    }
-
-    public void setPuntos(String puntos) {
-        this.puntos = puntos;
+    public Response(String licencePoints) {
+        this.licencePoints = licencePoints;
     }
 }
